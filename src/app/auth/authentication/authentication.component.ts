5eb27b4f569b5008a0168ae9fc53fc87921ad7fc
tushar rai <31855837+tusharrai5893@users.noreply.github.com>
2022-08-25 02:08:59 +0530

    sign in data fetch from dom is complete sending it to server is left

--- src/app/auth/authentication/authentication.component.ts
+++ src/app/auth/authentication/authentication.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SignInReqPayload } from './requestPayload/reqSignInPayload';
 
 @Component({
   selector: 'app-authentication',
@@ -7,20 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticationComponent implements OnInit {
 
-  constructor() { }
+  signInReqPayload!: SignInReqPayload;
+  signInForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.initSiginPayload(this.signInForm);
+  }
 
   ngOnInit(): void {
+    this.signInForm = this.getSignInDataFromDOM();
   }
-  onSignIn(e: Event) {
-    e.preventDefault();
+  areYouOneOfUs(e: Event) {
+    const signIn = document.getElementById("signIn");
+    const signUp = document.getElementById('signUp');
+    signIn?.classList.add("hidden");
+    signUp?.classList.add("block");
+  }
+  goAheadSignin(e: Event) {
+    const signUp = document.getElementById('signUp');
+    const signIn = document.getElementById("signIn");
+    signUp?.classList.add("hidden");
+    signIn?.classList.add("block");
+  }
+  doSignIn() {
+    const { username, password } = this.initSiginPayload(this.signInForm);
 
-    // const signIn = document.getElementById("signIn");
-    // signIn?.classList.add("hidden")
 
-    // const signUp = document.getElementById('signUp');
-    // signUp?.classList.remove("hidden");
-    // signUp?.classList.add("block");
 
   }
 
-}
+  getSignInDataFromDOM(): FormGroup {
+    return this.fb.group({
+      username: ["", [Validators.required, Validators.minLength(5)]],
+      secret: ["", [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  initSiginPayload(signInForm: FormGroup | undefined): SignInReqPayload {
+    return this.signInReqPayload = {
+      username: signInForm?.value.username,
+      password: signInForm?.value.secret,
+    };
+
+  }
+}// class ends
+
+
+
+
---
+++ src/app/auth/authentication/requestPayload/reqSignInPayload.ts
@@ -0,0 +1,4 @@
+export interface SignInReqPayload {
+    username: string,
+    password: string
+}
